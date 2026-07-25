582a22660548e25cc58c83949c0eb3b76431cadf
Eris <capcon432@gmail.com>
2026-07-25 11:18:21 -0600

    Made level 5 and added a couple new tiles

--- _Globals/Scenes/_Scenes.gd
+++ _Globals/Scenes/_Scenes.gd
@@ -9,6 +9,7 @@ const SCENES: Dictionary[String, PackedScene] = {
 	"Level 0":preload("uid://co1sgdv2x4nyr"), # Base level!! Do not use outside of testing!!
 	"Level 1":preload("uid://dhwaorb401jhg"),
 	"Level 2":preload("uid://bs0xsclp1lodr"),
+	"Level 5":preload("uid://boabyoymc6yd2"),
 }
 
 func swap_scene(scene_name: String) -> void:
