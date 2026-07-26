d48e16b027637001c3887f5fe50f0c89e4ea3256
bnhrl <benjamin.haroldsen@gmail.com>
2026-07-25 22:28:10 -0600

    Fixed BoostPads, added part of Level9

--- Game/BoostPads/boost_pad.gd
+++ Game/BoostPads/boost_pad.gd
@@ -4,19 +4,24 @@
 	set(new):
 		direction = new
 		refresh()
-@export var speed := 1000.0
+@export var speed := 100.0
 
 func _ready() -> void:
-	$Area/Collision.polygon = polygon
 	refresh()
+	if Engine.is_editor_hint(): return
+	
+	$Area/Collision.polygon = polygon
 
+@onready var area: Area2D = $Area
+func _process(_delta: float) -> void:
+	if Engine.is_editor_hint(): return
+	
+	for body in area.get_overlapping_bodies():
+		if body is Player:
+			body.momentum += Vector2.from_angle(deg_to_rad(direction)) * speed
+			body.velocity += Vector2.from_angle(deg_to_rad(direction)) * speed
+		elif body is Enemy:
+			body.velocity += Vector2.from_angle(deg_to_rad(direction)) * speed
 
 func refresh() -> void:
 	material.set_shader_parameter("rotation", deg_to_rad(direction))
-
-func _on_area_body_entered(body: Node2D) -> void:
-	if body is Player:
-		body.momentum += Vector2.from_angle(deg_to_rad(direction)) * speed
-		body.velocity += Vector2.from_angle(deg_to_rad(direction)) * speed
-	elif body is Enemy:
-		body.velocity += Vector2.from_angle(deg_to_rad(direction)) * speed
