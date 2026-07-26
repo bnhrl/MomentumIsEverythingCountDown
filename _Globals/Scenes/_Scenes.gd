aaa7a0a63f6f43f1e6a1a0ca9016575258643dc3
Eris <capcon432@gmail.com>
2026-07-25 21:54:12 -0600

    Made level 7

--- _Globals/Scenes/_Scenes.gd
+++ _Globals/Scenes/_Scenes.gd
@@ -13,7 +13,7 @@ const SCENES: Dictionary[String, PackedScene] = {
 	"Level 4":preload("uid://boabyoymc6yd2"),
 	"Level 5":preload("uid://bccx6wix5f4si"),
 	"Level 6":preload("uid://cxvriwd1raqxy"),
-	#"Level 7":preload(""),
+	"Level 7":preload("uid://cfga04bf50nus"),
 	"Level 8":preload("uid://dqqupmkumb3s0"),
 	#"Level 9":preload(""),
 	#"Level 10":preload(""),
